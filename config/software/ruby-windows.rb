54b72325baffbb3c9274aae83846dc465452653c
Sean Porter <portertech@gmail.com>
2013-01-22 12:08:54 -0500

    fixed ruby source url

--- config/software/ruby-windows.rb
+++ config/software/ruby-windows.rb
@@ -4,7 +4,7 @@ version "1.9.3-p374"
 
 relative_path "ruby-#{version}-i386-mingw32"
 
-source :url => "http://rubyforge.org/frs/download.php/76528/ruby-#{version}-i386-mingw32.7z",
+source :url => "http://rubyforge.org/frs/download.php/76707/ruby-#{version}-i386-mingw32.7z",
        :md5 => "7056392b8c2ae00cc9e7bdc7b82ead55"
 
 build do
