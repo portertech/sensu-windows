617502f360af931e5563fc4c7840f20d63b16d0c
Sean Porter <portertech@gmail.com>
2013-01-22 12:04:04 -0500

    wrong ruby md5

--- config/software/ruby-windows.rb
+++ config/software/ruby-windows.rb
@@ -5,7 +5,7 @@ version "1.9.3-p374"
 relative_path "ruby-#{version}-i386-mingw32"
 
 source :url => "http://rubyforge.org/frs/download.php/76528/ruby-#{version}-i386-mingw32.7z",
-       :md5 => "386ca038d4766972e08de05e50e1cda0"
+       :md5 => "7056392b8c2ae00cc9e7bdc7b82ead55"
 
 build do
   command "robocopy . #{install_dir}\\embedded\\ /MIR", :returns => [0, 1]
